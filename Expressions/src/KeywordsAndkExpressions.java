58a9d19750b2ae5e18640b94a027184aac521b9d
Gounaya <o.gounaya@gmail.com>
2018-06-10 13:52:53 +0200

    add a little function for testing the functionnality of Conditions

---
+++ Expressions/src/KeywordsAndkExpressions.java
@@ -0,0 +1,21 @@
+public class KeywordsAndkExpressions {
+
+    public static void main(String[] args) {
+
+        //a mile is equal to 1.609344
+        double kilometres =(10 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+        //In the following code that I will type belon, write down that parts of the code
+        //are expressions
+        int score = 100;
+        if(score > 99){
+            System.out.println("you got the high score!");
+            score = 100;
+        }
+    }
+}
